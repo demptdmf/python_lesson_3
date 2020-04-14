5796ebd2e852506bf5343c386930b123c650596c
vadim.trunin <demptdmf@bk.ru>
2020-04-14 19:11:10 +0300

    clear

--- homework_3.py
+++ homework_3.py
@@ -1,13 +1,40 @@
 f = open('text.txt')
 text = f.read()
+print('1) Методами строк очистить текст от знаков препинания:')
+text = text.replace(',', '')
+text = text.replace('.', '')
+text = text.replace('?', '')
+text = text.replace('!', '')
+text = text.replace('«', '')
+text = text.replace('»', '')
+text = text.replace(':', '')
+text = text.replace(';', '')
+text = text.replace('—', '')    # Хотел сделать в единой строке через or/and/& — но все условия не выполнялись. Сделал вручную, можно как-то сразу всё?
+print(text)                     # Появились двойные пробелы благодаря этой очистке. Ошибки в этом нет, т.к. шло 3 символа подряд, но как убрать двойные пробелы?
+print()
+
+
+print('2) Сформировать list со словами (split):')
+text = text.split()
+print(type(text))
+print()
+
+
+print('3) Gривести все слова к нижнему регистру (map);')
+text = list(map(lambda x: x.lower(), text))
+print(text)
+print()
+
+
+print('4) получить из list пункта 3 dict, ключами которого являются слова, а значениями их количество появлений в тексте;')
+# если скопировать значение листа и присвоить ему dict — то пишет ошибку:
+# SyntaxError: Non-UTF-8 code starting with '\xd0' in file homework_3.py on line 28, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details
 
 
 
 
 
-
-
-
+# print('5) Вывести 5 наиболее часто встречающихся слов (sort), вывести количество разных слов в тексте (set).')
 
 
 
--- string.py
+++ string.py
@@ -1,53 +1,72 @@
 # # --------------- Тип данных СТРОКА  (str) ---------------
 #
 #     # Инициализация
+print('------ ИНИЦИАЛИЗАЦИЯ ------ ')
+
 temp_str = 'Mark Auto Volvo'
-# print(temp_str)
-# # temp_str = ""
-#
-#
-#
-#     # Обращение к символам
+print(temp_str)
+print()
 
-# Вывод строки посимвольно (символ на каждой строчки)
-# for i in range(len(temp_str)):
-#     print(temp_str[i])
 
-# Срезы
-# print(temp_str[1:4])    # 1 — начать со второго символа, 0 — с начала, 4 — до 3 включительно.
-# print(temp_str[1:-3])   # -3 — это отрезать 3 символа с конца
+    # Обращение к символам
+print('------ ОБРАЩЕНИЕ К СТРОКАМ ------ ')
+print('Вывод строки посимвольно (символ на каждой строчки)')
+for i in range(len(temp_str)):
+    print(temp_str[i])
+print()
+
+print('Срезы')
+print(temp_str[1:4])    # 1 — начать со второго элемента, 0 — с начала, 4 — до 3 включительно.
+print(temp_str[1:-3])   # -3 — это отрезать 3 символа с конца
+
 
     # Функции со строками
+print('------ ФУНКЦИИ СО СТРОКАМИ ------ ')
+
 print(temp_str, len(temp_str))      # len — выводит количество символов в строке
 print(type(len))
+print()
+
 
     # Операции со строками
+print('------ ОПЕРАЦИИ СО СТРОКАМИ ------ ')
 temp_str_2 = 'Mercedes'
 print(temp_str + temp_str_2)    # Сложить строки, сложить данные, но лучше использовать форматирование.
 print(temp_str_2 * 2)           # Помножить строки
+print()
 
 
     # Форматирование строк
+print('------ ФОРМАТИРОВАНИЕ СТРОК ------ ')
+
 brand = 'Volvo'
 price = 1.5
 car = f'Марка: {brand} Цена: {price}'
 print(car)
+print()
 
 
     # Методы
+print('------ МЕТОДЫ ------ ')
 # SPLIT — разделение одной большой строки на подстроки
+
+print('Метод SPLIT — разделение одной большой строки на подстроки')
 print(temp_str.split())
 
 cars = 'volvo,Audi,Lada'        # Если не будет пробелов — будет считаться одной строчкой
 print(cars.split())             # Вывод будет: ['volvo,audi,lada']
 print(cars.split(','))          # Вывод будет: ['volvo', 'audi', 'lada']
+print()
 
 # Методы форматирования строк
+print('Методы форматирования строк')
 print(cars.upper())     # Верхний регистр VOLVO,AUDI,LADA
 print(cars.title())     # Первая буквая большая Volvo,Audi,Lada
 print(cars.lower())     # Нижний регистр volvo,audi,lada
+print()
 
 # Замена подстроки в строке
+print('Замена подстроки в строке')
 email_adress = 'Mail: _mail_'
 email = 'my@gmail.com'
 print(email_adress.replace('_mail_',email))     # _mail_ заменяем на значение переменной EMAIL благодаря .replace
