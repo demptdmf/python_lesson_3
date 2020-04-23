5fbe023c371edf827ad76c34a2fc8597bc755e88
vadim.trunin <demptdmf@bk.ru>
2020-04-23 22:12:27 +0300

    upd

--- homework 3.1.py
+++ homework 3.1.py
@@ -17,7 +17,7 @@ print()
 '''3) привести все слова к нижнему регистру (map);'''
 text_list = list(map(lambda letters: letters.lower(), text_list))
 print(text_list)
-print(type(text_list))
+print(len(text_list), type(text_list))
 print()
 
 
@@ -26,6 +26,7 @@ dict_text = {}                                  # Создаем пустой с
 for word in text_list:                          # В ЦИКЛЕ БЕРЕТСЯ ПЕРВОЕ СЛОВО, ОБХОДИТСЯ ПО СПИСКУ,
     dict_text[word] = text_list.count(word)     # СЧИТАЕТСЯ КОЛ-ВО ПОВТОРЕНИЙ, ЗАПИСЫВАЕТСЯ В СЛОВАРЬ ПОД КЛЮЧЕМ С СООТВЕТСВУЮЩИМ ИМЕНЕМ И ПЕРЕХОДИТ К СЛЕДУЩЕМУ ЭЛЕМЕНТУ
 print(dict_text)
+print(len(dict_text))
 print()
 '''ДЛЯ СЛОВА В СПИСКЕ ЗАПИСЫВАТЬ В СЛОВАРЬ СЛОВО С КОЛИЧЕСТВОМ ПОВТОРЕНИЙ ЭТОГО СЛОВА В СПИСКЕ'''
 
@@ -38,14 +39,21 @@ print(text_list[:5])
 print()
 
 '''вывести количество разных слов в тексте (set).'''
+
+print(len(text_list), '— элементов в списке')
+print(len(set(text_list)), '— элементов в множестве')
+
 similar_count = 0
 for value in dict_text.values():
     if value == 1:
         similar_count += 1
-print(similar_count, 'Одинаковых слов')
+print(similar_count, '— одинаковых слов')
 
 different_count = 0
 for value in dict_text.values():
     if value > 1:
         different_count += 1
-print(different_count, 'Разные слова')
+print(different_count, '— разных слов')
+
+
+)
---
+++ homework 3.2.py
@@ -0,0 +1,28 @@
+'''Самое короткое и красивое решение'''
+f = open('text.txt')
+text = f.read()
+
+symbols = ['.', ',', ';', ':', '!', '?', '«', '»', '—']
+for symbols in symbols:
+    text = text.replace(symbols, '')
+print()
+
+text = text.lower()
+text = text.split()
+print(text)
+print()
+
+dict_ = {}
+for word in text:
+    dict_[word] = text.count(word)
+
+dict_tolist = list(dict_.items())
+print('Количество повторений слов:', dict_tolist)
+
+dict_tolist.sort(key=lambda i: i[1], reverse = True)
+top_five = dict_tolist[0:5]
+print('Пять самых частых слов: ', top_five)
+
+words_set = set(text)
+words_quantity = len(words_set)
+print('Количество разных слов:', words_quantity)
